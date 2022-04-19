384f987c8a11a719bee2624facb3e44127d8cb04
jeffreyally <jeffreyally@gmail.com>
2022-04-19 17:51:08 +0000

    update

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -46,6 +46,7 @@ export const Navbar = () => {
 		
 
 	</div>
+
 </nav>
 
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
