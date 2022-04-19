44155502898785b7f2afa00b2532cb8fd5742bcf
jeffreyally <jeffreyally@gmail.com>
2022-04-19 19:07:37 +0000

    update

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -29,25 +29,25 @@ export const Navbar = () => {
 														<Link to="/" style={{ color: '#B388EB ', fontFamily: 'Coustard, serif' }}>Home</Link>
 													</div>
 												</div>
-												<div class="col">
+												<div class="col-lg">
 													<div class="nav-item me-5">
 
 														<Link to="/" style={{ color: '#B388EB ', fontFamily: 'Coustard, serif' }}>Menu</Link>
 													</div>
 												</div>
-												<div class="col">
+												<div class="col-lg">
 													<div class="nav-item me-5">
 
 														<Link to="/" style={{ color: '#B388EB ', fontFamily: 'Coustard, serif' }}>About</Link>
 													</div>
 												</div>
-												<div class="col">
+												<div class="col-lg">
 													<div class="nav-item me-5">
 
 														<Link to="/" style={{ color: '#B388EB ', fontFamily: 'Coustard, serif' }}>FAQ</Link>
 													</div>
 												</div>
-												<div class="col">
+												<div class="col-lg">
 													<div class="nav-item me-5">
 
 														<Link to="/" style={{ color: '#B388EB ', fontFamily: 'Coustard, serif' }}>Contact</Link>
