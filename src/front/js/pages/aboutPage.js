64e7e18e91a97323d20527a86321b22cb41804ba
jeffreyally <jeffreyally@gmail.com>
2022-04-20 14:50:11 +0000

    update

--- src/front/js/pages/aboutPage.js
+++ src/front/js/pages/aboutPage.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
+// style maybe style={{borderStyle:'solid',
+ // borderColor:'white',
+  //boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'
+//}}
 export const AboutPage = () => {
 
 return(
     <>
     <h1>this is the about page</h1>
 
-    
+    <div class="card" style={{width: '18rem'}}>
+  <img src="..." class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
     </>
 )
 }
