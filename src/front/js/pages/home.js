a86335edd3f5d7e967c87c21b9fd9a8629f1d7e6
jeffreyally <jeffreyally@gmail.com>
2022-04-20 13:13:37 +0000

    update

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -7,6 +7,7 @@ import springFlowers from "../../img/springflowers.jpeg"
 import BabyShower from "../../img/babyshower.jpeg"
 import wedding1 from "../../img/wedding1.jpeg"
 import wedding2 from "../../img/wedding2.jpeg"
+import {HomePageCookieCarousel} from '../component/homePageCookieCarousel.js'
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
@@ -14,92 +15,11 @@ export const Home = () => {
 	return (
 
 		<div className="text-center" style={{ backgroundColor: 'ghostwhite' }}>
-			
 			<br></br>
+			<HomePageCookieCarousel/>
 
-			<div id="carouselExampleInterval" class=" carousel slide carousel carousel-dark" data-bs-ride="carousel" >
-				<div class="carousel-inner">
-					<div class="carousel-item active" data-bs-interval="90000" style={{ position: 'relative' }}>
 
-						<div class="container">
-							<div class="row">
-
-								<div class="col-12 d-flex justify-content-center ">
-									<img src={springFlowers}class="d-block" alt="..."
-										style={{ maxWidth: '100%', height: '65vh', borderStyle:'solid', borderColor:'black' }} />
-								</div>
-
-							</div>
-						</div>
-
-					</div>
-					<div class="carousel-item" data-bs-interval="90000" style={{ position: 'relative' }}>
-						<div class="container">
-							<div class="row">
-							
-
-								<div class="col-12 d-flex justify-content-center on your column div">
-									<img src= {springURL} class="d-block" alt="..."
-										style={{ maxWidth: '100%', height: '65vh', borderStyle:'solid', borderColor:'black' }} />
-								</div>
-
-							</div>
-						</div>
-
-					</div>
-					<div class="carousel-item" data-bs-interval="90000" style={{ position: 'relative' }}>
-						<div class="container">
-							<div class="row">
-							
-
-								<div class="col-12 d-flex justify-content-center on your column div">
-									<img src= {BabyShower} class="d-block" alt="..."
-										style={{ maxWidth: '100%', height: '65vh', borderStyle:'solid', borderColor:'black' }} />
-								</div>
-
-							</div>
-						</div>
-
-					</div>
-					<div class="carousel-item" data-bs-interval="90000" style={{ position: 'relative' }}>
-						<div class="container">
-							<div class="row">
-							
-
-								<div class="col-12 d-flex justify-content-center on your column div">
-									<img src= {wedding1} class="d-block" alt="..."
-										style={{ maxWidth: '100%', height: '65vh', borderStyle:'solid', borderColor:'black' }} />
-								</div>
-
-							</div>
-						</div>
-
-					</div>
-					<div class="carousel-item" data-bs-interval="90000" style={{ position: 'relative' }}>
-						<div class="container">
-							<div class="row">
-							
-
-								<div class="col-12 d-flex justify-content-center on your column div">
-									<img src= {wedding2} class="d-block" alt="..."
-										style={{ maxWidth: '100%', height: '65vh', borderStyle:'solid', borderColor:'black' }} />
-								</div>
-
-							</div>
-						</div>
-
-					</div>
-
-				</div>
-				<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-									<span class="carousel-control-prev-icon" aria-hidden="true" style={{ color: 'black' }}></span>
-									<span class="visually-hidden">Previous</span>
-								</button>
-				<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-					<span class="carousel-control-next-icon" aria-hidden="true"></span>
-					<span class="visually-hidden">Next</span>
-				</button>
-			</div>
+			
 			<h3><span style={{ color: '#B388EB' }}>B388EB floral lavender</span>,
 				<span style={{ color: '#F7AEF8' }}>F7AEF8 plum something</span>,
 				<span style={{ color: '#FDC5F5' }}> FDC5F5cotton candy</span>,
@@ -135,3 +55,88 @@ export const Home = () => {
 
 	);
 };
+
+
+let carouselcode = `<div id="carouselExampleInterval" class=" carousel slide carousel carousel-dark" data-bs-ride="carousel" >
+<div class="carousel-inner">
+	<div class="carousel-item active" data-bs-interval="90000" style={{ position: 'relative' }}>
+
+		<div class="container">
+			<div class="row">
+
+				<div class="col-12 d-flex justify-content-center ">
+					<img src={springFlowers}class="d-block" alt="..."
+						style={{ maxWidth: '100%', height: '65vh', borderStyle:'solid', borderColor:'black' }} />
+				</div>
+
+			</div>
+		</div>
+
+	</div>
+	<div class="carousel-item" data-bs-interval="90000" style={{ position: 'relative' }}>
+		<div class="container">
+			<div class="row">
+			
+
+				<div class="col-12 d-flex justify-content-center on your column div">
+					<img src= {springURL} class="d-block" alt="..."
+						style={{ maxWidth: '100%', height: '65vh', borderStyle:'solid', borderColor:'black' }} />
+				</div>
+
+			</div>
+		</div>
+
+	</div>
+	<div class="carousel-item" data-bs-interval="90000" style={{ position: 'relative' }}>
+		<div class="container">
+			<div class="row">
+			
+
+				<div class="col-12 d-flex justify-content-center on your column div">
+					<img src= {BabyShower} class="d-block" alt="..."
+						style={{ maxWidth: '100%', height: '65vh', borderStyle:'solid', borderColor:'black' }} />
+				</div>
+
+			</div>
+		</div>
+
+	</div>
+	<div class="carousel-item" data-bs-interval="90000" style={{ position: 'relative' }}>
+		<div class="container">
+			<div class="row">
+			
+
+				<div class="col-12 d-flex justify-content-center on your column div">
+					<img src= {wedding1} class="d-block" alt="..."
+						style={{ maxWidth: '100%', height: '65vh', borderStyle:'solid', borderColor:'black' }} />
+				</div>
+
+			</div>
+		</div>
+
+	</div>
+	<div class="carousel-item" data-bs-interval="90000" style={{ position: 'relative' }}>
+		<div class="container">
+			<div class="row">
+			
+
+				<div class="col-12 d-flex justify-content-center on your column div">
+					<img src= {wedding2} class="d-block" alt="..."
+						style={{ maxWidth: '100%', height: '65vh', borderStyle:'solid', borderColor:'black' }} />
+				</div>
+
+			</div>
+		</div>
+
+	</div>
+
+</div>
+<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true" style={{ color: 'black' }}></span>
+					<span class="visually-hidden">Previous</span>
+				</button>
+<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+	<span class="carousel-control-next-icon" aria-hidden="true"></span>
+	<span class="visually-hidden">Next</span>
+</button>
+</div>`
