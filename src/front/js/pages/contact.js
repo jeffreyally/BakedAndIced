cede7915e68e89c789a4a3ce363765d4506c7e5c
jeffreyally <jeffreyally@gmail.com>
2022-04-24 19:37:52 +0000

    update

--- src/front/js/pages/contact.js
+++ src/front/js/pages/contact.js
@@ -7,6 +7,11 @@ export const Contact = () => {
   return (
 
     <div class="container">
+      <br></br>
+      <p class="text-center" style={{
+        fontFamily: 'Bubblegum Sans, cursive',
+        fontSize: '3rem'
+      }}><i>CONTACT US</i></p>
 
       <div class="row d-flex justify-content-center  mt-3 no-gutters">
 
@@ -20,24 +25,27 @@ export const Contact = () => {
 
         </div>
 
-        <div class="col-md-5 d-flex justify-content-center align-items-center" style={{ borderStyle: 'solid' }}>
+        <div class="col-md-7 d-flex justify-content-center align-items-center  p-0" style={{
+          backgroundColor: '#F7AEF8',
+        }}>
           <div class=" d-flex ">
 
             <form target="_blank" action="https://formsubmit.co/7801e72aca46261f4264461e4ada688a" method="POST">
-              <div class="form-group">
-                <div class="form-row">
-                  <div class="col">
-                    <input type="text" name="name" class="form-control" placeholder="Full Name" required />
-                  </div>
-                  <div class="col">
-                    <input type="email" name="email" class="form-control" placeholder="Email Address" required />
-                  </div>
-                </div>
-              </div>
-              <div class="form-group">
-                <textarea placeholder="Your Message" class="form-control" name="message" rows="5" required></textarea>
-              </div>
-              <button type="submit" class="btn btn-primary">Submit Form</button>
+              <label for="yourname" class="form-label">Name</label>
+              <input type="text" id="yourname" name="name" class="form-control" placeholder="Full Name" required />
+
+              <br></br>
+              <label for="youremail" class="form-label">Email address</label>
+
+              <input type="email" id="youremail" name="email" class="form-control" placeholder="Email Address" required />
+              <br></br>
+              <label for="yourmessage" class="form-label">Message</label>
+
+
+
+              <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
+
+              <button type="submit" class="btn btn-sm btn-primary">Submit Form</button>
             </form>
 
 
