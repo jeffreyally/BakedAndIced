ab43defe405e7dde89b823e77fb5fb3db4aeceb4
jeffreyally <jeffreyally@gmail.com>
2022-04-25 19:33:26 +0000

    update

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -39,7 +39,7 @@ export const Footer = () => {
        
         <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
          
-        <h5 class="text-uppercase mb-4">Opening hours</h5>
+        <h5 class="text-uppercase mb-4">Business hours</h5>
   
   <table class="table  text-yellow">
     <tbody class="font-weight-normal">
@@ -60,20 +60,22 @@ export const Footer = () => {
         
         </div>
         
-        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+        <div class="col-md-3 mx-auto mb-md-2 mb-4">
           
-          <h6 class="text-uppercase fw-bold mb-4">
+          <h6 class="text-uppercase fw-bold mb-4 ">
             Contact
           </h6>
           
-          <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
+          
           <p>
-            <i class="fas fa-envelope me-3"></i>
+            <i class="fas fa-envelope me-3 fa-lg"></i>
             info@example.com
           </p>
-          <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
-          <p><i class="fab fa-instagram"></i> Follow Us On Instagram</p>
-        </div>
+          <p><i class="fas fa-phone me-3 fa-lg"></i> + 01 234 567 88</p>
+          <p><i class="fab fa-instagram me-3 fa-lg" style={{fontWeight:'bold'}}></i> Follow Us On Instagram</p>
+          
+          
+                  </div>
         
       </div>
     
